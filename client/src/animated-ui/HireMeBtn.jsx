5be1711470c87c74889f41795c22becaa8840312
Janarthanans1 <janarthanansmj@gmail.com>
2025-06-14 18:19:06 +0530

    no changes

--- client/src/animated-ui/HireMeBtn.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const HireMeBtn = () => {
-  return <div>HireMeBtn</div>;
-};
-
-export default HireMeBtn;
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import { Volleyball } from "lucide-react";
-import HireMeBtn from "../animated-ui/HireMeBtn";
+import { Volleyball, ArrowUpRight } from "lucide-react";
+
 import { Logs } from "lucide-react";
 import HamburgarMenu from "../animated-ui/HamburgarMenu";
 
@@ -8,7 +8,7 @@ const Navbar = () => {
   const [open, setOpen] = useState(false);
   const menus = ["Home", "About", "Services", "Skill", "Projects", "Contact"];
   return (
-    <div className="w-full text-white font-black flex justify-center">
+    <div className="w-full text-white font-black flex justify-center items-center">
       <nav className="w-19/20 h-[70px] px-8 rounded-full flex justify-between items-center bg-black/50 shadow-2xl fixed top-5">
         <div id="logo">
           <Volleyball className="w-8 h-8 md:w-10 md:h-10" />
@@ -24,8 +24,15 @@ const Navbar = () => {
           </ul>
         </div>
 
-        <div className="flex gap-4" id="hire me btn">
-          <HireMeBtn />
+        <div className="group flex items-center gap-4" id="hire me btn">
+          <a href="#Contact">
+            <div className="w-40 h-fit bg-black/60 group-hover:pl-3 pl-5 pr-3 py-2 rounded-2xl flex justify-center items-center gap-2">
+              <p className="group-hover:hidden min-w-fit">Hire Me</p>
+              <div className="group-hover:w-full z-20 transition-all duration-75 bg-red-600 w-8 h-8 flex justify-center items-center  rounded-xl">
+                <ArrowUpRight />
+              </div>
+            </div>
+          </a>
           <div className="md:hidden" onClick={() => setOpen(!open)}>
             <Logs />
           </div>
